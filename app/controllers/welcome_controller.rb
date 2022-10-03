4d5be64dc83f9982691cb13cc71125019c9d0312
tmyrick <travis.myrick@dutchie.com>
2022-10-02 19:12:56 -0700

    make fixes, add faq

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@
 
 class WelcomeController < ApplicationController
   def index
-    @google_maps_url = 'https://goo.gl/maps/nBAxNAsmPSS2'
+    @google_maps_url = 'https://g.page/LolomaLodge?share'
     @cool_earth_url = 'https://www.coolearth.org/'
   end
 end
