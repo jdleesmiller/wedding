0ca77d6fd103f02b194bbc0dbde381adc9d015ea
tmyrick <travis.myrick@dutchie.com>
2023-03-06 19:25:52 -0800

    add registry cta and jquery for smooth scroll

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -22,4 +22,35 @@ $(function () {
     });
 
     $( "#schedule tr td:nth-child(2)" ).addClass( "text-right" );
+
+    // Define selector for selecting
+    // anchor links with the hash
+    let anchorSelector = 'a[href^="#"]';
+
+    $(anchorSelector).on('click', function (e) {
+
+        // Prevent scrolling if the
+        // hash value is blank
+        e.preventDefault();
+
+        // Get the destination to scroll to
+        // using the hash property
+        let destination = $(this.hash);
+
+        // Get the position of the destination
+        // using the coordinates returned by
+        // offset() method and subtracting 50px
+        // from it.
+        let scrollPosition
+            = destination.offset().top - 50;
+
+        // Specify animation duration
+        let animationDuration = 500;
+
+        // Animate the html/body with
+        // the scrollTop() method
+        $('html, body').animate({
+            scrollTop: scrollPosition
+        }, animationDuration);
+    });
 })
